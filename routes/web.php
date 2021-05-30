62399bd9069862b97048b8589fa317915a60646e
Dariusz Swoszowski <polishcomposer@gmail.com>
2021-05-30 22:51:43 +0100

    Posts CRUD

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-/*
-Route::get('/users/{id}/{name}', function($id, $name){
-    return 'this is user '.$name.' with an ID of '.$id.'.';
-});
-*/
-
 Route::get('/', 'App\Http\Controllers\PagesController@index');
 Route::get('/about', 'App\Http\Controllers\PagesController@about');
 Route::get('/services', 'App\Http\Controllers\PagesController@services');
-/*
-Route::get('/about', function(){
-    return view('pages.about');
-});
-*/
-//Route::get('/user', [PagesController::class, 'index']);
+
+Route::resource('posts', 'App\Http\Controllers\PostsController');
